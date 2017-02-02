217aaefe52d5ac54c8e2caf6144089b63a5ce743
Cinque <Cinque_CR@hotmail.com>
2017-02-01 19:11:08 -0600

    Segundo Commit

---
+++ Semana4/Semana4/JavaScript.js
@@ -0,0 +1,24 @@
+﻿/*Cuadrado*/
+function Area_Cuadrado(Num1) {
+    return Math.pow(Num1,2) 
+}
+
+/*Rectangulo*/
+function Area_Rectangulo(Num1,Num2) {
+    return parseInt(Num1) * parseInt(Num2)
+}
+
+/*Triangulo*/
+function Area_Triangulo(Num1, Num2) {
+    return parseInt(parseInt(Num1) * parseInt(Num2)/ParseInt(2))
+}
+
+/*Rombo*/
+function Area_Rombo(Num1, Num2) {
+    document.getElementById("Practica").innerHTML = (parseInt(Num1) * parseInt(Num2)) / 2
+}
+
+/*Paralelogramo*/
+function Area_Triangulo(Num1, Num2) {
+    document.getElementById("Practica").innerHTML = parseInt(Num1) * parseInt(Num2)
+}
